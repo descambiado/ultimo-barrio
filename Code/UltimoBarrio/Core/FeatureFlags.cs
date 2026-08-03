f9329f95c48599242386a2e2ad33f73bfba3a9c0
DavidHernan3 <fp_hernandezdavid@ciudadjardin.org>
2026-08-03 18:00:36 +0200

    chore(flags): disable non-essential systems for Alpha 0.1 runtime repair

--- Code/UltimoBarrio/Core/FeatureFlags.cs
+++ Code/UltimoBarrio/Core/FeatureFlags.cs
@@ -4,8 +4,10 @@ namespace UltimoBarrio.Core
 {
     public static class FeatureFlags
     {
-        public static bool EnableEconomy { get; set; } = true;
-        public static bool EnableCombat { get; set; } = true;
-        public static bool EnableRaids { get; set; } = true;
+        public static bool EnableEconomy { get; set; } = false;
+        public static bool EnableCombat { get; set; } = false;
+        public static bool EnableRaids { get; set; } = false;
+        public static bool EnableWorldClock { get; set; } = false;
+        public static bool EnableAI { get; set; } = false;
     }
 }
